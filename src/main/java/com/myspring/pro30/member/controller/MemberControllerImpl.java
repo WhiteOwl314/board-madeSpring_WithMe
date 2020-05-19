e0be68bfd33a88162abd0b66d4e7ab25b3460ecb
ParkSeongju <parksj914@naver.com>
2020-05-19 15:14:09 +0900

    interceptor-preHandle-getViewName

--- src/main/java/com/myspring/pro30/member/controller/MemberControllerImpl.java
+++ src/main/java/com/myspring/pro30/member/controller/MemberControllerImpl.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.myspring.pro30.member.service.MemberService;
+import com.myspring.pro30.member.vo.MemberVO;
 
-@Controller
+@Controller("memberController")
 public class MemberControllerImpl implements MemberController{
 	
 	@Autowired
